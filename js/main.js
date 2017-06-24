5649ca668bebcc26b9963b0003f3d7e71559a519
jteklak <jteklak@gmail.com>
2017-06-24 11:37:53 +0200

    przepisanie funkcji reużywalana

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@
 
 var wszystkieZgody = document.getElementById("wszystkie-zgody");
 
+var zgodaMarketingowa1 = document.getElementById("zgoda-marketingowa-1");
+var zgodaMarketingowa2 = document.getElementById("zgoda-marketingowa-2");
+
 var wszystkieCheckboxy = document.querySelectorAll("input[type=checkbox]");
 
-wszystkieZgody.onchange = function(){
- console.log("kliknięty");
- console.log(this.checked);
+ function stanCheckboxa(){
+ zgodaMarketingowa2.checked = this.checked;
 }
 
 
+
+wszystkieZgody.onchange = stanCheckboxa;
 
 
 

6fbf6cd7d888531c525530ee525b0d1c3ac713e3
jteklak <jteklak@gmail.com>
2017-06-24 11:58:02 +0200

    sprawdzanie czy imie jest puste

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
 "use strict"
 
 var wszystkieZgody = document.getElementById("wszystkie-zgody");
-
 var zgodaMarketingowa1 = document.getElementById("zgoda-marketingowa-1");
 var zgodaMarketingowa2 = document.getElementById("zgoda-marketingowa-2");
+var wyslijBtn = document.getElementById("wyslij");
 
-var wszystkieCheckboxy = document.querySelectorAll("input[type=checkbox]");
+var inputImie = document.getElementById("name");
+var inputEmail = document.getElementById("email");
 
  function stanCheckboxa(){
+     
  zgodaMarketingowa1.checked = this.checked;
  zgodaMarketingowa2.checked = this.checked;
      
  zgodaMarketingowa1.disabled = this.checked;
  zgodaMarketingowa2.disabled = this.checked;
+     
 }
+
 wszystkieZgody.onchange = stanCheckboxa;
 
-document.getElementById("wyslij").addEventListener("click", walidujFormularz);
+function walidujFormularz (event) {
+    event.preventDefault();
+    
+    if(inputImie.value == ""){
+       console.log("pole jest puste");
+       }
+    
+    
+}
 
 
+wyslijBtn.addEventListener("click", walidujFormularz);
+
+    
 
 

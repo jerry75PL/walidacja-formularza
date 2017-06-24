ed22e5ed513501fa7275a6f5086dfe72a3d62906
jteklak <jteklak@gmail.com>
2017-06-24 11:45:01 +0200

    pobieranie stanu wyslij

--- js/main.js
+++ js/main.js
@@ -8,17 +8,16 @@ var zgodaMarketingowa2 = document.getElementById("zgoda-marketingowa-2");
 var wszystkieCheckboxy = document.querySelectorAll("input[type=checkbox]");
 
  function stanCheckboxa(){
+ zgodaMarketingowa1.checked = this.checked;
  zgodaMarketingowa2.checked = this.checked;
+     
+ zgodaMarketingowa1.disabled = this.checked;
+ zgodaMarketingowa2.disabled = this.checked;
 }
-
-
-
 wszystkieZgody.onchange = stanCheckboxa;
 
+document.getElementById("wyslij").addEventListener("click", walidujFormularz);
 
 
 
-
-console.log(wszystkieZgody);
-console.log(wszystkieCheckboxy);
 

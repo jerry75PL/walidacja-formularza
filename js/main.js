fdac39ab881478bd07bc1b5c5334eb0de0d1d772
jteklak <jteklak@gmail.com>
2017-06-24 13:31:46 +0200

    na razie koniec

--- js/main.js
+++ js/main.js
@@ -8,46 +8,51 @@ var wyslijBtn = document.getElementById("wyslij");
 var inputImie = document.getElementById("name");
 var inputEmail = document.getElementById("email");
 
- function stanCheckboxa(){
-     
- zgodaMarketingowa1.checked = this.checked;
- zgodaMarketingowa2.checked = this.checked;
-     
- zgodaMarketingowa1.disabled = this.checked;
- zgodaMarketingowa2.disabled = this.checked;
-     
+var wiadomosc = document.getElementById("wiadomosc")
+
+function stanCheckboxa() {
+
+    zgodaMarketingowa1.checked = this.checked;
+    zgodaMarketingowa2.checked = this.checked;
+
+    zgodaMarketingowa1.disabled = this.checked;
+    zgodaMarketingowa2.disabled = this.checked;
+
 }
 
 wszystkieZgody.onchange = stanCheckboxa;
 
-function walidujFormularz (event) {
-   
-    var bladWalidacjiMsg = document.createElement('p');
-    bladWalidacjiMsg.innerHTML = "Bład";
-    inputImie.parentNode.insertBefore(bladWalidacjiMsg, inputImie.nextSibling);
-    
-    if(inputImie.value.trim() == ""){
-         event.preventDefault();
-        console.log("pole imie puste");
-        
-              }
-    
-    if(inputEmail.value.trim() == ""){
-         event.preventDefault();
-        console.log("pole Email jest puste");
-      
-       }
-    if(! zgodaMarketingowa1.checked) {
-         event.preventDefault();
-        console.log("zgoda nie jest zaznaczona");
-       
-       }
-    
+function walidujFormularz(event) {
+
+    wiadomosc.innerHTML = '<ul id="wiadomosc"></ul>';
+
+    if (inputImie.value.trim() == "") {
+        var msgImie = document.createElement("li");
+        msgImie.innerHTML = "Wpisz Imie!"
+        wiadomosc.appendChild(msgImie);
+        event.preventDefault();
+
+    }
+
+    if (inputEmail.value.trim() == "") {
+        var msgEmail = document.createElement("li");
+        msgEmail.innerHTML = "Wpisz Email!"
+        wiadomosc.appendChild(msgEmail);
+        event.preventDefault();
+
+
+    }
+
+    if (!zgodaMarketingowa1.checked) {
+        var msgZgodaMarketingowa = document.createElement("li");
+        msgZgodaMarketingowa.innerHTML = "Zaznacz zgodę!"
+        wiadomosc.appendChild(msgZgodaMarketingowa);
+        event.preventDefault();
+
+
+    }
+
 }
 
 
 wyslijBtn.addEventListener("click", walidujFormularz);
-
-    
-
-
